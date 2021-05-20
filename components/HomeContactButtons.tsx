2a92607ec63f140c2d88e66fac048e96498cbd22
Artie Fischer <artie@curetechnologies.us>
2021-05-19 21:44:44 -0600

    refactor: clean up button components

--- components/HomeContactButtons.tsx
+++ components/HomeContactButtons.tsx
@@ -4,13 +4,7 @@ import React from "react";
 type NavItem = {
   title: string;
   url: string;
-  styleSector: {
-    width: number;
-    backgroundColor: string;
-    color: string;
-    borderRadius: number;
-    marginBottom: number;
-  };
+  styleSector: any[];
 };
 
 const HomeContactButtons = () => {
@@ -18,24 +12,24 @@ const HomeContactButtons = () => {
     {
       title: "Github Repo",
       url: "https://github.com/rafischer1/med-timer-2",
-      styleSector: styles.buttonContainerThree,
+      styleSector: [styles.github, styles.button],
     },
     {
       title: "LinkedIn",
       url: "https://www.linkedin.com/in/robert-a-fischer/",
-      styleSector: styles.buttonContainerOne,
+      styleSector: [styles.linkedin, styles.button],
     },
     {
       title: "Instagram",
       url: "https://www.instagram.com/peacemang/",
-      styleSector: styles.buttonContainerTwo,
+      styleSector: [styles.instagram, styles.button],
     },
   ];
   return (
     <SafeAreaView>
-      {navItems.map((link) => {
+      {navItems.map((link, index) => {
         return (
-          <View style={link.styleSector}>
+          <View style={link.styleSector} key={index}>
             <Button
               color={"black"}
               title={link.title}
@@ -51,26 +45,15 @@ const HomeContactButtons = () => {
 const navTo = ({ url }): any => Linking.openURL(url);
 
 const styles = StyleSheet.create({
-  buttonContainerOne: {
-    width: 210,
+  button: { color: "white", borderRadius: 10, marginBottom: 10, width: 210 },
+  linkedin: {
     backgroundColor: "#78DCEF",
-    color: "white",
-    borderRadius: 10,
-    marginBottom: 10,
   },
-  buttonContainerTwo: {
-    width: 210,
+  instagram: {
     backgroundColor: "#78BFEF",
-    color: "white",
-    borderRadius: 10,
-    marginBottom: 10,
   },
-  buttonContainerThree: {
-    width: 210,
+  github: {
     backgroundColor: "#78EFE4",
-    color: "white",
-    borderRadius: 10,
-    marginBottom: 10,
   },
 });
 
