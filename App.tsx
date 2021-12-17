f6220e918f7a9cb1103de62df9cf6429238ffcc1
Artie Fischer <artie@curetechnologies.us>
2021-12-17 10:55:55 -0700

    chore: fix import

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import Tabs from "./navigation/MainTabNavigator";
 import { NavigationContainer } from "@react-navigation/native";
